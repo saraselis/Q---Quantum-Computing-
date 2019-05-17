64484205899893876975cbfc415cacdddb342226
Sara Selis <38225430+saraselis@users.noreply.github.com>
2019-05-17 18:58:56 -0300

    Pedra, Papel e Tesoura
    Pedra, Papel e Tesoura com aleatoriedade quântica. Q#

---
+++ pedra_papel_tesoura/pedra_papel_tesoura/Driver.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+using Microsoft.Quantum.Simulation.Core;
+using Microsoft.Quantum.Simulation.Simulators;
+
+namespace Quantum.pedra_papel_tesoura
+{
+    class Driver
+    {
+        static void Main(string[] args)
+        {
+            using (var qsim = new QuantumSimulator())
+            {
+                Console.WriteLine("Digite sua escolha: 1- Pedra, 2- Papel, 3- Tesoura");
+                int a = Convert.ToInt32(Console.ReadLine());
+                int b = Convert.ToInt32(Play.Run(qsim)).Result;
+            }
+        }
+    }
+}
