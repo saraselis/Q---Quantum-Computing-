3bb1e8d511200925e32cb9367d8ecb3acccbd318
Sara Selis <38225430+saraselis@users.noreply.github.com>
2019-05-28 12:32:07 -0300

    correção do bug de retorno

---
+++ pedra-papel-tesoura-aleatoriedade/QSharpApplication1/QSharpApplication1/Operations.qs
@@ -0,0 +1,35 @@
+﻿namespace Quantum.PedraPapelETesoura
+{
+    open Microsoft.Quantum.Intrinsic;
+    open Microsoft.Quantum.Canon;
+    
+    operation Set(q : Qubit, v : Result) : Unit{
+		if(M(q) != v){
+			X(q);
+		}
+	}
+	
+	operation Play() : Int{
+		mutable r = 0;
+		using(q = Qubit()){
+			Set(q, Zero);
+			H(q);
+			R(PauliY, 0.3255, q);
+			let res = M(q);
+			if(res == Zero){
+				set r = 0;
+			}else{
+				Set(q, Zero);
+				H(q);
+				let resF = M(q);
+				if(resF == Zero){
+					set r = 1;
+				}else{
+					set r = 2;
+				}
+			}
+			Set(q, Zero);
+		}
+		return r;
+}
+}
---
+++ pedra-papel-tesoura-aleatoriedade/pedra_papel_tesoura_aletoriedade/pedra_papel_tesoura_aletoriedade/Driver.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+using Microsoft.Quantum.Simulation.Core;
+using Microsoft.Quantum.Simulation.Simulators;
+
+namespace Quantum.PedraPapelETesoura
+{
+    class Driver
+    {
+        static void ShowChoice(int a)
+        {
+            switch (a)
+            {
+                case 0:
+                    Console.WriteLine("Pedra");
+                    break;
+                case 1:
+                    Console.WriteLine("Papel");
+                    break;
+                case 2:
+                    Console.WriteLine("Tesoura");
+                    break;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            using (var qsim = new QuantumSimulator())
+            {
+                Console.WriteLine("Digite sua escolha (0 = Pedra, 1 = Papel, 2 = Tesoura):");
+                int a = Convert.ToInt32(Console.ReadLine());
+                int b = Convert.ToInt32(Play.Run(qsim).Result);
+
+                Console.WriteLine("Você: ");
+                ShowChoice(a);
+                Console.WriteLine("Computador: ");
+                ShowChoice(b);
+
+                int c = a - b;
+
+                if (c == -2 || c == 1)
+                {
+                    Console.WriteLine("Você ganhou!");
+                }
+                else if (c == 0)
+                {
+                    Console.WriteLine("Você empatou!");
+                }
+                else if (c == -1 || c == 2)
+                {
+                    Console.WriteLine("Você perdeu!");
+                }
+            }
+        }
+    }
+}
